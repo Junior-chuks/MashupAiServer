2431607c8aa8bd12a7eae37a81304c7c4e141e63
phngcam022 <phngcam022@student.wethinkcode.co.za>
2023-11-04 23:55:27 +0200

    frontend

--- Mtn-JAH-APP/src/main/java/org/server/Main.java
+++ Mtn-JAH-APP/src/main/java/org/server/Main.java
@@ -1,12 +1,15 @@
 package org.server;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import org.json.JSONObject;
 import persistence.Job;
 import persistence.JobService;
 import persistence.SchoolService;
-
 
 public class Main implements Runnable{
 
@@ -17,11 +20,12 @@ public class Main implements Runnable{
     public static void main(String[] args) {
         Main api = new Main().initialise();
         api.run();
+
     }
 
 
     public void startServer(){
-        server.start("192.168.1.100",PORT);
+        server.start("192.168.1.107",PORT);
     }
 
     public void stopServer(){
@@ -48,7 +52,15 @@ public class Main implements Runnable{
     }
 
     public Javalin initHTTPServer(){
-        return Javalin.create().get("/jobs/{place}",this::getJobs).get("/jobs",this::getAllJobs).get("/schools",this::getSchools);
+        Javalin app =  Javalin.create(javalinConfig -> {
+            javalinConfig.enableCorsForAllOrigins();
+        });
+
+
+                app.get("/jobs/{place}",this::getJobs);
+                app.get("/jobs",this::getAllJobs);
+                app.get("/schools",this::getSchools);
+                return app;
     }
 
     public void getJobs(Context context){
@@ -56,7 +68,16 @@ public class Main implements Runnable{
     }
 
     public void getAllJobs(Context context){
-        context.json(Jobs.getJobs());
+        JSONObject json = new JSONObject();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Jobs.getJobs().forEach(job -> {
+            try {
+                json.put(job.getTitle(),objectMapper.writeValueAsString(job));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        context.json(json.toMap());
     }
 
     public void getSchools(Context context){
